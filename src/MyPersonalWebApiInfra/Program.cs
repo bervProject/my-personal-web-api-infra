0b67cc992e7f32b2d1de22acd84526dae4ccd867
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-11-20 16:45:52 +0700

    Migrate to CDK

---
+++ src/MyPersonalWebApiInfra/Program.cs
@@ -0,0 +1,25 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyPersonalWebApiInfra
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new MyPersonalWebApiInfraStack(app, "MyPersonalWebApiInfraStack", new StackProps
+            {
+                Env = new Amazon.CDK.Environment
+                {
+                    Account = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
+                    Region = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION"),
+                }
+                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+            });
+            app.Synth();
+        }
+    }
+}
